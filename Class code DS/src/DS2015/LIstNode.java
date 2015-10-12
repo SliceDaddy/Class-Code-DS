d7dcb3ec1ef836a58f9ad9e877d87d2ee974128d
SliceDaddy <rslipher@saintjoe.edu>
2015-10-12 12:36:39 -0400

    New code

---
+++ Class code DS/src/DS2015/LIstNode.java
@@ -0,0 +1,17 @@
+package DS2015;
+
+public class ListNode extends Node{
+
+	ListNode next ;
+
+	  public ListNode() {
+		  super();
+//		  next = null;
+	  }
+	  public ListNode(int a) {
+		super(a);
+		// TODO Auto-generated constructor stub
+	}
+
+	  
+}
