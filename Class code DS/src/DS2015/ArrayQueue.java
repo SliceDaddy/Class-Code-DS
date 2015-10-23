f55d95aa39450e20069fabda4460c0d4574de8e3
SliceDaddy <rslipher@saintjoe.edu>
2015-10-23 12:13:59 -0400

    code changed

--- Class code DS/src/DS2015/ArrayQueue.java
+++ Class code DS/src/DS2015/ArrayQueue.java
@@ -1,47 +1,54 @@
 package DS2015;
 
 public class ArrayQueue {
-	private final int CAPACITY = 7;
+	private int CAPACITY = 7;
 	private int [] data;
-	private int headIndex =0, tailIndex = 0;
-	private int size;
+	private int headIndex =0, tailIndex=0;
+	private int size =0;
 	
 	// good habit to clearly define the constructor
 	public ArrayQueue(){
-		data = new int[7];
-		headIndex =0;
-		tailIndex = 0;
-		size = 0;
+		data = new int[CAPACITY];
+	}
+
+	public ArrayQueue(int capacity){
+		data = new int[capacity];
+		this.CAPACITY=capacity;
 	}
 	
 	public int getSize(){
 		return size;
 	}
-	
 	public boolean isEmpty(){
 		return size == 0;
 	}
 	
 	public void enQueue(int x){
-		if (size == this.CAPACITY){
-			throw new IllegalStateException("Queue is full");
+		if(size == this.CAPACITY){
+			throw new IllegalStateException("Queue is full, cannot add anymore");
 		}
-		tailIndex = (tailIndex+1) % this.CAPACITY;
 		data[tailIndex ] = x;
-		size++;
+		tailIndex = (tailIndex+1) % this.CAPACITY;
+		size++;		
 	}
 	public int deQueue(){
-		if(size == 0)
-			throw new IllegalStateException("Queue is empty , cannot deQueue");
+		if(isEmpty())
+			throw new IllegalStateException("Queue is empty , cannot dequeue");
 		int result = data[headIndex];
-		headIndex = (headIndex+1) & this.CAPACITY;
+		headIndex = (headIndex+1) % this.CAPACITY;
+		size--;
 		return result;
-		}
+	}
 	
 	public int peekFirst(){
-		
-		return 0;
+		if(size == 0)
+			throw new IllegalStateException("Queue is empty , cannot find any value");
+		return data[headIndex];
+	}
+	public void printQueue(){
+		for(int i =0;i<size;i++)
+			System.out.print(data[(headIndex+i)%this.CAPACITY]+"\t");		
+		System.out.println();
 	}
 }
-
 
